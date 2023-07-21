1a699dc0ba17fd53f21aae31e47c3dbbe7023950
gavin ted <youremail@yourdomain.com>
2023-07-21 13:11:22 +0800

    fix

--- public/config.js
+++ public/config.js
@@ -1,26 +1,28 @@
 var speedBase = 0.7;
 var Config = {
 	hackMode: 1, // 0:模拟手速, 1: 零失误
-	emps: 5,
+	emps: 5, // 初始 EMP 数量
 	empsPerWave: 1, // 每關 送的 EMP 数量
 	maxEmpsPerWave: 5, // 每關 送的 EMP 数量 的最大值
-	maxHistoryLength: 30,
-	showMapWord: true,
-	showSetWord: true,
-	speakEnglish: true,
+	maxHistoryLength: 30, // 最大历史记录长度
+	showMapWord: true, // 是否顯示翻譯單字
+	showSetWord: true, // 是否顯示目標單字
+	speakEnglish: true, // 是否發音英文
 	speak:{
 		rate: 1, // 確認速度 0.1 ~ 2
 		pitch: 1, // 確認音調 0 ~ 2
 		volume: 1, // 確認音量 0.1 ~ 1
 	},
-	speakMap: true,
+	speakMap: true, // 是否發音翻譯
 	mapSpeak:{
 		rate: 1, // 確認速度 0.1 ~ 2
 		pitch: 1, // 確認音調 0 ~ 2
 		volume: 0.7, // 確認音量 0.1 ~ 1
 		lang:'zh-TW',
 	},
-	getWordWithLength: function (l,ig) {
+	
+	// ====================================
+	getWordWithLength: function (l,ig) { // 單字產生器
 		const w = String.fromCharCode('a'.charCodeAt(0) + (Math.random() * 26).floor());
 		if(l === 1) return w;
 
@@ -39,6 +41,8 @@ var Config = {
 		}
 		return w;
 	},
+
+	// ====================================
 	EntityEnemyMissle: {
 		speed: 30 * speedBase,
 		wordLength: {
