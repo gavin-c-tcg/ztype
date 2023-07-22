c70703413bea0981945551e7c98913b6f9408454
gavin ted <youremail@yourdomain.com>
2023-07-22 12:58:30 +0800

    調整速度

--- public/game_setting.js
+++ public/game_setting.js
@@ -35,7 +35,6 @@ var Game2 = {
 	...ConfigBase,
 	statisticType: "kills", // 殺敵數
 
-	spawnWait: 2, // 生成敵人間隔
 	logWord: true, // 是否顯示單字
 	logWithMap: false, // 是否顯示翻譯單字
 	showSetWord: false, // 是否顯示目標單字
@@ -44,18 +43,24 @@ var Game2 = {
 	// speakMap: true, // 是否發音翻譯
 	mapWordPosition: 1, // 1: 顯示在單字上方 2: 顯示在單字下方
 
+
+
+	speedIncrease: 1, // 敵人速度 基數倍數
+	spawnWait: 2, // 生成敵人間隔
+	
+
 	// ====================================
 
 	EntityEnemyMine: { // 小型敵人
 		...ConfigBase.EntityEnemyMine,
-		speed: 40, // 速度
+		// speed: 40, // 速度
 		$angleType: 0, // 0:  隨機其他模式形式 1: 飛向玩家 2: 隨機飛向
 		$initCount: 5, // 初始數量
 		$incEvery: 2, // 每Ｎ關加一個
 	},
 	EntityEnemyDestroyer: { // 中型敵人(會生小怪)
 		...ConfigBase.EntityEnemyDestroyer,
-		speed: 30, // 速度
+		// speed: 30, // 速度
 		$shootTimeBase: 2.5, // 射擊間隔倍數，越大越射越慢
 		$initCount: 0, // 初始數量
 		$incEvery: 5, // 每Ｎ關加一個
@@ -67,6 +72,7 @@ var Game2 = {
 	},
 	EntityEnemyMissle: { //[子彈] 中型敵人生的子彈
 		...ConfigBase.EntityEnemyMissle,
+		// speed: 50, // 速度
 	},
 	EntityEnemyBullet: { // [子彈] 大型敵人 單字閃彈槍
 		...ConfigBase.EntityEnemyBullet,
